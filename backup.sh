86eb02783ded3b3710da459de42c80fbd2060409
Andres Gonzalez <andresgz@users.noreply.github.com>
2017-07-23 14:02:12 -0400

    Allow to specify folder to store and backup full folder

--- backup.sh
+++ backup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 DAY=$(date +"%a")
 HOSTNAME=$1
-LOCAL_BACKUPS_PATH=${2-"/home/ubuntu/backups/auto"}
-DESTINATION_BACKUPS_PATH=${3-"/mnt/disk/backups/auto"}
-PROJECTS_FOLDER=${4-"/srv/apps"}
+FOLDER_TO_STORE=${2-DAY}
+LOCAL_BACKUPS_PATH=${3-"/home/ubuntu/backups/auto"}
+DESTINATION_BACKUPS_PATH=${4-"/mnt/disk/backups/auto"}
+PROJECTS_FOLDER=${5-"/srv/apps"}
 
 for f in $PROJECTS_FOLDER/*; do
   if [ -d "$f" ]; then
@@ -11,14 +12,14 @@ for f in $PROJECTS_FOLDER/*; do
      project_name="${PWD##*/}"
      if [ "$project_name" != "certbot" ]; then
         echo $project_name
-        mkdir -p $LOCAL_BACKUPS_PATH/$DAY
+        mkdir -p $LOCAL_BACKUPS_PATH/$FOLDER_TO_STORE
         wp db export $LOCAL_BACKUPS_PATH/$project_name.sql
-  echo $LOCAL_BACKUPS_PATH/$DAY/files_$project_name.tgz
-  tar czvf $LOCAL_BACKUPS_PATH/$DAY/files_$project_name.tgz wp-content/uploads -P
-  tar czvf $LOCAL_BACKUPS_PATH/$DAY/db_$project_name.tgz $LOCAL_BACKUPS_PATH/$project_name.sql
-  ssh backup mkdir -p $DESTINATION_BACKUPS_PATH/$HOSTNAME/$DAY
-  scp $LOCAL_BACKUPS_PATH/$DAY/files_$project_name.tgz backup:$DESTINATION_BACKUPS_PATH/$HOSTNAME/$DAY/
-  scp $LOCAL_BACKUPS_PATH/$DAY/db_$project_name.tgz backup:$DESTINATION_BACKUPS_PATH/$HOSTNAME/$DAY/
+  echo $LOCAL_BACKUPS_PATH/$FOLDER_TO_STORE/files_$project_name.tgz
+  tar czvf $LOCAL_BACKUPS_PATH/$FOLDER_TO_STORE/files_$project_name.tgz . -P --exclude='*.tgz' --exclude='*.sql' --exclude='*.gz' --exclude='*.zip' --exclude='*.tar' --exclude='*.sql' --exclude='*.zip' --exclude='wp-content/cache/*' --exclude='.git/*'
+  tar czvf $LOCAL_BACKUPS_PATH/$FOLDER_TO_STORE/db_$project_name.tgz $LOCAL_BACKUPS_PATH/$project_name.sql
+  ssh backup mkdir -p $DESTINATION_BACKUPS_PATH/$HOSTNAME/$FOLDER_TO_STORE
+  scp $LOCAL_BACKUPS_PATH/$FOLDER_TO_STORE/files_$project_name.tgz backup:$DESTINATION_BACKUPS_PATH/$HOSTNAME/$FOLDER_TO_STORE/
+  scp $LOCAL_BACKUPS_PATH/$FOLDER_TO_STORE/db_$project_name.tgz backup:$DESTINATION_BACKUPS_PATH/$HOSTNAME/$FOLDER_TO_STORE/
      fi
   fi
 
